d175ab5178dc763dcc1e2b881d6583a6c67dad94
lilyoh <lilyoh628@gmail.com>
2024-02-09 11:54:51 +1100

    navigate with programmatic way

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -3,9 +3,18 @@ import { useRouter } from 'next/router';
 function ClientProjectsPage() {
 	const router = useRouter();
 
+	function loadProjectHandler() {
+		// loading data...
+		router.push({
+			pathname: '/clients/[id]/[clientprojectid]',
+			query: { id: 'max', clientprojectid: 'projecta' },
+		});
+	}
+
 	return (
 		<div>
 			<h1>The Projects of a Given Client: {router.query.id}</h1>
+			<button onClick={loadProjectHandler}>Load Project A</button>
 		</div>
 	);
 }
